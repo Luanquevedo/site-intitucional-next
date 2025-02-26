1952759dc2936af123afc7df6c290bcf5b1ba5bc
Luan Meira Quevedo <luan.quevedo@hotmail.com>
2025-02-26 19:09:58 -0300

    Adicionado todas seções responsivas pendente otimização para tablet e criação do nav/footer e form

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -3,27 +3,22 @@ import Head from 'next/head';
 
 const Contact = () => {
   return (
-    <div>
+    <div >
       <Head>
         <title>Áreas de Atendimento - Tempero da Grazzy</title>
         <meta name="description" content="Confira as áreas de atendimento da Tempero da Grazzy, incluindo as principais regiões de São Paulo e outras cidades do Brasil como Brasília, Recife, Ceará, Minas Gerais e Florianópolis." />
         <meta name="keywords" content="áreas de atendimento, São Paulo, Brasília, Recife, Ceará, Minas Gerais, Florianópolis, serviços de buffet, catering, eventos" />
       </Head>
 
-      <section>
+      <section className='Contact'>
         <header>
-          <h1>Áreas de Atendimento</h1>
+          <h2>Áreas de Atendimento</h2>
         </header>
         
-        <article>
-          <h2>Localidades atendidas</h2>
+        <article className='Contact__Content'>
           <p><strong>São Paulo</strong> – Jardins, Higienópolis, Zonas Sul, Leste e Oeste</p>
-          <p><strong>Outras regiões atendidas:</strong> <strong>Brasília, Recife, Ceará, Minas Gerais, Florianópolis</strong></p>
+          <p><strong>Brasília, Recife, Ceará, Minas Gerais, Florianópolis</strong></p>
         </article>
-
-        <footer>
-          <p>Se a sua cidade não estiver listada, entre em contato conosco para mais informações sobre a expansão das nossas áreas de atendimento.</p>
-        </footer>
       </section>
     </div>
   );
